a3e1501de50c66cabb71f64dcafc2febde6183c3
MartinSerran0 <127114873+MartinSerran0@users.noreply.github.com>
2024-06-18 10:54:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const caixaPerguntas =document.querySelector(".caixa-perguntas");
 const caixaAlternativas =document.querySelector(".caixa-alternativas");
 const caixaResultado =document.querySelector(".caixa-resultado");
 const textoResultado =document.querySelector(".texto-resultado");
+
+const perguntas = [
+    enunciado: "Quem Ganhou A Copa Do Mundo De 2022 ",
+    alternativas: [
+"Argentina",
+"França"
+]
+   ];`
